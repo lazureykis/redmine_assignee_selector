2c05098fff9befa7770a1ac2730ca17af951f9ef
Pavel Lazureykis <lazureykis@gmail.com>
2016-02-03 12:01:33 +0300

    UPD js refactored

--- assets/javascripts/assignee_selector.js
+++ assets/javascripts/assignee_selector.js
@@ -1,25 +1,29 @@
 $(document).ready(function() {
-  var change_assignee_btn = "<a class='icon icon-edit show_assignee_selector' href='#'> </a>";
-  var button_container = " <span class='assignee_selector_button'>"+change_assignee_btn+"</span>";
-  $('.issue .attribute.assigned-to .value').append(button_container);
+  var assigned_to_id_value = $('.issue .attribute.assigned-to .value')
+  assigned_to_id_value.append(" <a class='icon icon-edit show_assignee_selector' href='#'> </a>");
 
-  $('.assignee_selector_button .show_assignee_selector').on('click', function(e){
+  $('.show_assignee_selector').on('click', function(e){
     e.preventDefault();
 
     var select_control = $('select#issue_assigned_to_id').clone();
-    select_control.attr('id', null);
-    select_control.attr('class', null);
-    select_control.attr('tabindex', null);
-    select_control.attr('aria-hidden', null);
-    $('.issue .attribute.assigned-to .value').empty().append(select_control);
+    select_control.attr('id', null)
+                  .attr('class', null)
+                  .attr('tabindex', null)
+                  .attr('aria-hidden', null);
 
-    $('.issue .attribute.assigned-to .value select').select2();
-    $('.issue .attribute.assigned-to .value select').on('change', function() {
-      console.log("Changed: " + $(this).val());
+    assigned_to_id_value.empty().append(select_control);
+
+    if (select_control.select2 !== undefined) {
+      select_control.select2();
+      select_control.select2('open');
+    }
+
+    select_control.on('change', function() {
       $('#issue_assigned_to_id').val($(this).val());
       $('form.edit_issue').submit();
-    });
 
-    $('.assignee_selector_control').show();
+      selected_username = $(this).find(':selected').text();
+      assigned_to_id_value.text("Saving '" + selected_username + "'...");
+    });
   });
 });
